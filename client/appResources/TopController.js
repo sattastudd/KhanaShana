3af882382381203e519e5d7ec19d6a2a962e4674
satish_mishra <sattasaphire@gmail.comm>
2015-02-04 03:16:27 +0530

    header changed

--- client/appResources/TopController.js
+++ client/appResources/TopController.js
@@ -13,4 +13,16 @@ function parentController($scope, DataStore, AppConstants, $location){
 		console.log('Going to Login Page');
 		$location.path('/login');
 	}
+
+
+	//code for header
+
+	$scope.searchCalled = function(){
+         $scope.searchActive = true;
+        $scope.$broadcast('newItemAdded');
+    }
+    $scope.searchDeactive = function(){
+         $scope.searchActive = false;   
+    }
+    $scope.searchPage = true;
 }
