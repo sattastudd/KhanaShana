90ab58a64507a20dfd0dc7559f44f1eda557c18d
satish_mishra <sattasaphire@gmail.comm>
2015-02-08 20:49:47 +0530

    enhanced user search result

--- client/appResources/controller/userSearchResultController.js
+++ client/appResources/controller/userSearchResultController.js
@@ -13,6 +13,11 @@ function searchResultController ($scope){
 
     console.log("inside search result page");
 
+    if(window.scrollTo(0, 400)){
+        $scope.customizeVisible = true;
+        console.log("its a miracle");
+    }
+
     //for making circle
 
    /* var c = document.getElementById("myCanvas");
