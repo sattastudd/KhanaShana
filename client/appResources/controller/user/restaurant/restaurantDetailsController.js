f994bfc75582c2e073423db1b790d5eaa34eefea
satish_mishra <sattasaphire@gmail.comm>
2015-05-11 01:48:55 +0530

    restaurant home page enhanced

--- client/appResources/controller/user/restaurant/restaurantDetailsController.js
+++ client/appResources/controller/user/restaurant/restaurantDetailsController.js
@@ -1,35 +1,34 @@
-define([], function () {
+define([], function() {
     function restaurantDetailsController($scope) {
     }
     return restaurantDetailsController;
 });
 
-function RestaurantDetailsController($scope, $route, $http, $routeParams) {
-    console.log($routeParams);
-    
-    $scope.rest = {};
-    
-    $scope.dishShortlisted = [];
-    
-    console.log('alpha to charlie');
-    
-    $http.get('node/public/restaurant/' + $routeParams.restSLug)
-        .success(function (data) {
-        console.log(data);
-        
-        $scope.rest.name = data.data.name;
-        //$scope.rest.img = data.data.img.full;
-        $scope.rest.menus = data.data.menu;
-        $scope.rest.img = data.data.img;
+function RestaurantDetailsController ($scope, $route, $http, $routeParams, $location){
+    console.log( $routeParams );
 
-    })
-        .error(function (data) {
-        console.log(data);
-    });
-    
+    $scope.rest = {};
+
+    $scope.dishShortlisted = [];
+
+    console.log('alpha to charlie');
+
+    $http.get('node/public/restaurant/' + $routeParams.restSLug)
+        .success( function( data ){
+            console.log(data );
+
+            $scope.rest.name = data.data.name;
+            //$scope.rest.img = data.data.img.full;
+            $scope.rest.menus = data.data.menu;
+
+        })
+        .error( function ( data ) {
+            console.log( data );
+        });
+
     console.log("i want this one");
     console.log($scope.rest.menus);
-    
+
     $scope.menuSelected = function (menu) {
         console.log(menu);
         $scope.menu = menu;
@@ -37,45 +36,55 @@ function RestaurantDetailsController($scope, $route, $http, $routeParams) {
         console.log(menu.items);
         //if (menu.items.price) {};
     }
-    
-    $scope.count = 1;
-    
-    $scope.itemAddedHalf = function (item) {
+
+    //$scope.count = 1;
+
+    $scope.itemAddedHalf = function(item){
         console.log(item.title);
         console.log(item.price.half);
-        
+        $scope.cartLoaded = true;
+
         var objectToPush = {
             dish : item.title,
             price : item.price.half,
-            quantity : 1
+            quantity : 1,
+            totalPrice : item.price.half,
+            value : "Half"
         }
         
-        $scope.dishShortlisted.push(objectToPush);
+        $scope.dishShortlisted.push( objectToPush );
 
         
     }
-    $scope.itemAddedFull = function (item) {
+    $scope.itemAddedFull = function(item){
         console.log(item);
-        
-        var objectToPush = {
-            dish : item.title,
-            price : item.price.full,
-            quantity : 1
-        }
-        
-        $scope.dishShortlisted.push(objectToPush);
+        $scope.cartLoaded = true;
+        //console.log($scope.dishShortlisted.quantity);
+
+            var objectToPush = {
+                dish : item.title,
+                price : item.price.full,
+                quantity : 1,
+                totalPrice : item.price.full,
+                value : "Full"
+            }
+            
+            $scope.dishShortlisted.push( objectToPush );
+            //$scope.dishDuplicacy = $scope.
     }
 
-    /*$scope.quantity = 1;
-    $scope.quantityIncreased = function(){
-        console.log('quantity increased');
-        $scope.quantity = $scope.quantity + 1;
+    $scope.quantityIncreased = function(dish){
+        console.log(dish);
+
+        dish.quantity = dish.quantity + 1;
+        dish.totalPrice = dish.quantity * dish.price;
     }
-    $scope.quantityDecreased = function(item){
-        $scope.quantity = $scope.quantity - 1;
-        if($scope.quantity === null || $scope.quantity === 0){
-            $scope.hideAddItem = true;
-        }
+    $scope.quantityDecreased = function(dish){
+
+        dish.quantity = dish.quantity - 1;
+        dish.totalPrice = dish.quantity * dish.price;
     }
-*/
+    $scope.proceeded = function(){
+        $location.path('/CheckOut');
+    }
 };
