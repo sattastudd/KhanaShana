ab27511333bcfb331f346c822070aead3b42cea2
satish_mishra <sattasaphire@gmail.comm>
2015-05-13 22:22:10 +0530

    changes done in restaurant home page

--- client/appResources/controller/user/restaurant/restaurantDetailsController.js
+++ client/appResources/controller/user/restaurant/restaurantDetailsController.js
@@ -44,6 +44,7 @@ function RestaurantDetailsController ($scope, $route, $http, $routeParams, $loca
         console.log(item.price.half);
         $scope.cartLoaded = true;
 
+        if(typeof $scope.dishShortlisted.quantity === 'undefined'){
         var objectToPush = {
             dish : item.title,
             price : item.price.half,
@@ -53,6 +54,9 @@ function RestaurantDetailsController ($scope, $route, $http, $routeParams, $loca
         }
         
         $scope.dishShortlisted.push( objectToPush );
+    }else{
+        console.log("its in else part");
+    }
 
         
     }
