3eabbe493310b78540e6196595f38c1657b48dd6
satish_mishra <sattasaphire@gmail.comm>
2015-06-03 02:39:31 +0530

    user profile page added

---
+++ client/appResources/controller/userProfileController.js
@@ -0,0 +1,10 @@
+define([], function() {
+    function userProfileController($scope) {
+    }
+    return userProfileController;
+});
+
+function profileController($scope){
+
+	console.log("inside profile");
+}
