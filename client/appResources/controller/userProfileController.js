4d40c42e1b9b05de0a8aa4cec5e9a47f5789ed65
satish_mishra <sattasaphire@gmail.comm>
2015-06-05 23:34:48 +0530

    user profile page enhanced

--- client/appResources/controller/userProfileController.js
+++ client/appResources/controller/userProfileController.js
@@ -7,4 +7,49 @@ define([], function() {
 function profileController($scope){
 
 	console.log("inside profile");
+
+	
+    $scope.order = [{
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }, {
+        orderNo: '123',
+        restName: 'Waah ji Waah',
+        cost: '250',
+        orderTime: '11:11 AM',
+        deliveredIn: '30 mins'
+    }];
 }
